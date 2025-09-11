ed3d5375c823c3ff9ddc3618d787dbfb9a21b353
helen <helen8335@gmail.com>
2025-09-11 11:38:09 -0500

    add AuthService.java

--- landbnb/src/main/java/com/labndbnb/landbnb/service/AuthService.java
+++ landbnb/src/main/java/com/labndbnb/landbnb/service/AuthService.java
@@ -1,6 +1,17 @@
 package com.labndbnb.landbnb.service;
 
-public class AuthService {
+import com.labndbnb.landbnb.dto.*;
 
+public interface AuthService {
 
+    AuthResponse register(UserRegistration request);
+
+    AuthResponse login(LoginRequest request);
+
+    void sendResetPasswordEmail(String email);
+
+    void resetPassword(ResetPasswordRequest request);
+
+    void changePassword(ChangePasswordRequest request);
 }
+
