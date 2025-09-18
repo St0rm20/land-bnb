f4c5fd63a97ac7db447368a06733330a61d0a113
Storm <167824736+St0rm20@users.noreply.github.com>
2025-09-18 09:18:58 -0500

    Create Notification.java
    Clase para administrar las notificaciones del usuario

---
+++ landbnb/src/main/java/com/labndbnb/landbnb/model/Notification.java
@@ -0,0 +1,34 @@
+package com.labndbnb.landbnb.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+   
+}
